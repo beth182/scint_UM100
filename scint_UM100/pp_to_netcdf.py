c25e3bfc72b2ff83ef0f6ab9dab10f7dce1cca81
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-03-14 17:01:15 +0000

    write a pp to nc file converter

---
+++ scint_UM100/pp_to_netcdf.py
@@ -0,0 +1,31 @@
+from os import listdir
+from os.path import isfile, join
+import os
+# import iris
+
+# Actual version of this script is on the RACC
+
+model = '100m'
+# model = '300m'
+# model = 'ukv'
+
+run = '20160512T1200Z'
+
+main_dir = '/storage/basic/micromet/Tier_processing/rv006011/UM100/'
+# main_dir = "//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/"
+
+pp_dir = main_dir + 'pp/' + run + '/' + model + '/'
+
+netcdf_dir = main_dir + 'netcdf/' + run + '/' + model + '/'
+
+# check if the write out dir exists
+if os.path.isdir(netcdf_dir) == False:
+    os.makedirs(netcdf_dir)
+
+file_list = [f for f in listdir(pp_dir) if isfile(join(pp_dir, f))]
+
+for file_path in file_list:
+    print(pp_dir + file_path)
+
+    cubes = iris.load(pp_dir + file_path)
+    iris.save(cubes, netcdf_dir + file_path.split('.')[0] + '.nc')
