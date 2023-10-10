0b770570c5d83b81cdb56f7d92570c3fb36a4697
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-10-10 16:12:17 +0100

    update plot with real world coords

--- scint_UM100/data_retreval/retreve_data_from_target_grids.py
+++ scint_UM100/data_retreval/retreve_data_from_target_grids.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import rasterio.plot
 import geopandas
+import matplotlib.colors as colors
 
 import iris
 import cartopy.crs as ccrs
@@ -216,6 +217,25 @@ fig = plt.figure(figsize=(10, 10))
 
 ax = fig.add_subplot(111, projection=ccrs.epsg(32631))
 
+# plot the land cover map
+landcover_raster_filepath='C:/Users/beths/OneDrive - University of Reading/Model_Eval/QGIS/Elliott/LandUseMM_7classes_32631.tif'
+landcover_raster = rasterio.open(landcover_raster_filepath)
+# color_list_lc = ["white", "dimgrey", "lightgrey", "deepskyblue", "lawngreen", "darkgreen", "limegreen", "olive"]
+color_list_lc = ["white", "black", "dimgrey", "white", "lightgrey", "lightgrey", "lightgrey", "lightgrey"]
+# make a color map of fixed colors
+cmap_lc = colors.ListedColormap(color_list_lc)
+bounds_lc = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+norm_lc = colors.BoundaryNorm(bounds_lc, cmap_lc.N)
+rasterio.plot.show(landcover_raster, ax=ax, cmap=cmap_lc, norm=norm_lc, interpolation='nearest', alpha=0.5)
+
+# plot paths
+scint_shp_file_path = 'D:/Documents/scint_plots/scint_plots/sa_position_and_lc_fraction/scint_path_shp/BCT_IMU.shp'
+df_12 = geopandas.read_file(scint_shp_file_path)
+df_12.plot(edgecolor='red', ax=ax, linewidth=3.0)
+
+
+
+
 # Plot observation SA
 # open SA file
 sa_file = 'D:/Documents/scint_UM100/scint_UM100/SA_134/BCT_IMU_15000_2016_134_12_00.tif'
@@ -261,11 +281,30 @@ proj_y = cube.coord("grid_latitude").points
 cs_nat = cube.coord_system()
 cs_nat_cart = cs_nat.as_cartopy_projection()
 
+
+sa_a_norm = (sa_a - np.nanmin(sa_a)) / (np.nanmax(sa_a) - np.nanmin(sa_a))
+sa_a_norm[np.isnan(sa_a_norm)] = 0
+
 im = ax.pcolormesh(proj_x,
                    proj_y,
                    a,
                    transform=cs_nat_cart,
-                   cmap='jet')
+                   cmap='jet',
+                   alpha=sa_a_norm)
+
+plt.colorbar(im)
+
+# limits which stay constant between and which suit both day's SAs
+ax.set_xlim(283000, 287000)
+ax.set_ylim(5711000, 5718000)
+
+# im = ax.pcolormesh(proj_x,
+#                    proj_y,
+#                    a,
+#                    transform=cs_nat_cart,
+#                    cmap='jet')
+
+
 
 """
 
