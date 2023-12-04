73ce9536728bdd44501fbfacbc1d9e204b4f1b9c
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-12-04 13:24:16 +0000

    Script for extracting LC

---
+++ scint_UM100/land_cover/get_lc_for_grids.py
@@ -0,0 +1,124 @@
+# imports
+import os
+import iris
+import cartopy.crs as ccrs
+from pyproj import Proj, transform
+import netCDF4 as nc
+import pandas as pd
+import numpy as np
+
+# get the location of the target grid in rw coords
+# convert to MO cords
+# get the LC fractions for that grid
+# save them as a file
+
+model = '100m'
+stash_code = 'm01s00i216'
+
+# get the location of the target grid in rw coords
+# existing csv:
+coord_lookup_csv = 'D:/Documents/scint_UM100/scint_UM100/grid_coords/grid_coord_lookup/grid_coords_' + model + '.csv'
+
+# read the csv
+coord_lookup_df = pd.read_csv(coord_lookup_csv)
+
+grid_coords = coord_lookup_df[coord_lookup_df.descrip == 'MID']
+
+# convert to MO cords
+# temp read in cube
+
+pp_file_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/UM100_ancillaries/london_100m/qrparm.veg.frac.urb2t'
+
+assert os.path.isfile(pp_file_path)
+
+# takes a long time
+cube = iris.load(pp_file_path, stash_code)[0]
+
+rot_pole = cube.coord('grid_latitude').coord_system.as_cartopy_crs()
+
+ll = ccrs.Geodetic()
+
+# convert coords
+inProj = Proj(init='epsg:32631')
+outProj = Proj(init='epsg:4326')
+
+
+# read nc file
+nc_file_path = "//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/netcdf/20160512T1200Z/UM100_ancillaries/qrparm_veg_frac_urb2t.nc"
+nc_file = nc.Dataset(nc_file_path)
+
+LC_array = nc_file.variables[stash_code]
+
+assert LC_array.shape[1] == LC_array.shape[2]
+
+lc_df_list = []
+
+for index, row in grid_coords.iterrows():
+    name = row.grid
+    print(int(name), ' / ', len(grid_coords))
+
+    # get coord
+    x = row.x
+    y = row.y
+
+    point = transform(inProj, outProj, x, y)
+
+    point_x = point[0]
+
+    if point_x < 0:
+        point_x = 360 + point_x
+
+    point_y = point[1]
+
+    target_xy = rot_pole.transform_point(point_x, point_y, ll)  # mid point
+
+    x_new = target_xy[0]
+    y_new = target_xy[1]
+
+    latitudes = cube[0].coord('grid_latitude')
+    longitudes = cube[0].coord('grid_longitude')
+
+    assert latitudes.shape[0] == longitudes.shape[0]
+
+    nearest_lat = latitudes.nearest_neighbour_index(y_new)
+    nearest_lon = longitudes.nearest_neighbour_index(x_new)
+
+    coord_tuple = (nearest_lat, nearest_lon)
+    i = coord_tuple[0]
+    j = coord_tuple[1]
+
+    if np.isnan(LC_array[:, i, j]).all():
+        pass
+    else:
+        assert np.isclose(sum(LC_array[:, i, j]), 1)
+
+    LC_grid = LC_array[:, i, j]
+
+    # 0 = broadleaf
+    # 1 = needleleaf
+    # 2 = C3
+    # 3 = C4
+    # 4 = shrubs
+    # 5 = lake
+    # 6 = soil
+    # 7 = ice
+    # 8 = canyon
+    # 9 = roof
+
+    # create a df with this info in
+
+    lc_df = pd.DataFrame.from_dict({'broadleaf': [LC_grid[0]], 'needleleaf': [LC_grid[1]], 'C3': [LC_grid[2]],
+     'C4': [LC_grid[3]], 'shrubs': [LC_grid[4]], 'lake': [LC_grid[5]],
+     'soil': [LC_grid[6]], 'ice': [LC_grid[7]], 'canyon': [LC_grid[8]],
+     'roof': [LC_grid[9]]})
+
+    lc_df.index = [name]
+
+    lc_df_list.append(lc_df)
+
+save_path = os.getcwd().replace('\\', '/') + '/'
+all_grids_df = pd.concat(lc_df_list)
+all_grids_df.to_csv(save_path + 'all_grids_lc.csv')
+print('CSV saved to: ' + save_path + 'all_grids_lc.csv')
+
+print('end')
