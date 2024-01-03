95c2aab60a219b4b0ad9eadd610554a1f686a546
beth182 <bethany.saunders@pgr.reading.ac.uk>
2024-01-03 20:00:26 +0000

    create plot

--- scint_UM100/land_cover/plot_urban_for_area.py
+++ scint_UM100/land_cover/plot_urban_for_area.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 import iris
 import cartopy.crs as ccrs
 
-
-
-
 stash_code = 'm01s00i216'
 
 pp_file_path_100 = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/UM100_ancillaries/london_' + '100m' + '/qrparm.veg.frac.urb2t'
@@ -53,8 +50,6 @@ proj_y_300 = extracted_300.coord("grid_latitude").points
 proj_x_ukv = extracted_ukv.coord("grid_longitude").points
 proj_y_ukv = extracted_ukv.coord("grid_latitude").points
 
-
-
 # load the nc files
 
 nc_file_path_100 = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/netcdf/20160512T1200Z/UM' + '100' + '_ancillaries/qrparm_veg_frac_urb2t.nc'
@@ -64,7 +59,6 @@ nc_file_path_ukv = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_pro
 assert os.path.isfile(nc_file_path_100)
 assert os.path.isfile(nc_file_path_300)
 assert os.path.isfile(nc_file_path_ukv)
-
 
 # read the nc files for vals
 nc_file_100 = nc.Dataset(nc_file_path_100)
@@ -76,12 +70,9 @@ urban_300 = nc_file_300.variables[stash_code][-1, :, :] + nc_file_300.variables[
 nc_file_ukv = nc.Dataset(nc_file_path_ukv)
 urban_ukv = nc_file_ukv.variables[stash_code][-1, :, :] + nc_file_ukv.variables[stash_code][-2, :, :]
 
-
 print('end')
 
-
-
-fig = plt.figure(figsize=(20,7))
+fig = plt.figure(figsize=(20, 7))
 ax = fig.add_subplot(131)
 im = ax.pcolormesh(proj_x_100,
                    proj_y_100,
@@ -90,25 +81,21 @@ im = ax.pcolormesh(proj_x_100,
                    vmax=1,
                    cmap='jet')
 
-
-
 ax2 = fig.add_subplot(132)
 im2 = ax2.pcolormesh(proj_x_300,
-                   proj_y_300,
-                   urban_300,
+                     proj_y_300,
+                     urban_300,
                      vmin=0,
                      vmax=1,
-                   cmap='jet')
-
+                     cmap='jet')
 
 ax3 = fig.add_subplot(133)
 im3 = ax3.pcolormesh(proj_x_ukv,
-                   proj_y_ukv,
-                   urban_ukv,
+                     proj_y_ukv,
+                     urban_ukv,
                      vmin=0,
                      vmax=1,
-                   cmap='jet')
-
+                     cmap='jet')
 
 ax.set_xticklabels([])
 ax.set_yticklabels([])
@@ -117,6 +104,17 @@ ax2.set_yticklabels([])
 ax3.set_xticklabels([])
 ax3.set_yticklabels([])
 
-plt.subplots_adjust(wspace=0, hspace=0)
+ax.set_xticks([])
+ax.set_yticks([])
+ax2.set_xticks([])
+ax2.set_yticks([])
+ax3.set_xticks([])
+ax3.set_yticks([])
+
+
+plt.subplots_adjust(wspace=0.01, hspace=0.01)
+
+save_path = os.getcwd().replace('\\', '/') + '/'
+plt.savefig(save_path + 'urban_frac.png', bbox_inches='tight', dpi=300)
 
 print('end')
