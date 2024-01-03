efee92ddaa033fbff12d519736573c94e4348f14
beth182 <bethany.saunders@pgr.reading.ac.uk>
2024-01-03 21:49:31 +0000

    move map alpha plots to its own file

--- scint_UM100/data_retreval/QH_alpha_plot.py
+++ scint_UM100/data_retreval/QH_alpha_plot.py
@@ -30,8 +30,8 @@ target_hours = [12]
 
 variable_name = 'upward_heat_flux_in_air'
 # model = '100m'
-model = '300m'
-# model = 'ukv'
+# model = '300m'
+model = 'ukv'
 
 run = '20160512T1200Z'
 levels = True
@@ -62,7 +62,6 @@ for target_hour in target_hours:
         if target_hour in existing_df.index:
 
             continue
-            # print('end')
         else:
             pass
     else:
@@ -95,7 +94,8 @@ for target_hour in target_hours:
     # look up grids for this hour
     # sa_grids_lookup_csv = 'D:/Documents/scint_UM100/scint_UM100/grid_coords/SA_grid_overlap/SA_UM100_grid_percentages.csv'
     # sa_grids_lookup_csv = 'D:/Documents/scint_UM100/scint_UM100/grid_coords/SA_grid_overlap/' + path + '_SA_UM100_grid_percentages.csv'
-    sa_grids_lookup_csv = 'D:/Documents/scint_UM100/scint_UM100/grid_coords/SA_grid_overlap/' + path + '_SA_UM' + model.split('m')[0] + '_grid_percentages.csv'
+    sa_grids_lookup_csv = 'D:/Documents/scint_UM100/scint_UM100/grid_coords/SA_grid_overlap/' + path + '_SA_UM' + \
+                          model.split('m')[0] + '_grid_percentages.csv'
 
     sa_grids_df = pd.read_csv(sa_grids_lookup_csv)
 
@@ -142,9 +142,11 @@ for target_hour in target_hours:
     # temp read in cube
 
     if model == 'ukv':
-        pp_file_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/pp/20160512T1200Z/' + model.split('m')[0] + '/umnsaa_pexptb023.pp'
+        pp_file_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/pp/20160512T1200Z/' + \
+                       model.split('m')[0] + '/umnsaa_pexptb023.pp'
     else:
-        pp_file_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/pp/20160512T1200Z/' + model.split('m')[0] + 'm/umnsaa_pexptb023.pp'
+        pp_file_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/UM100/pp/20160512T1200Z/' + \
+                       model.split('m')[0] + 'm/umnsaa_pexptb023.pp'
 
     assert os.path.isfile(pp_file_path)
 
@@ -267,7 +269,8 @@ for target_hour in target_hours:
             if (i, j) in lat_lon_tuples:
                 a[i, j] = var_array[i, j]
 
-                sa_val = float(target_grid_coords.loc[target_grid_coords['ind_tuples'] == (i, j)][str(target_hour).zfill(2)])
+                sa_val = float(
+                    target_grid_coords.loc[target_grid_coords['ind_tuples'] == (i, j)][str(target_hour).zfill(2)])
                 sa_a[i, j] = sa_val
 
     if np.isclose(100, np.nansum(sa_a)):
@@ -279,147 +282,117 @@ for target_hour in target_hours:
     weighted_av_a = np.nansum(weighted_a)
     a_weighted_percent = (weighted_a / np.nansum(weighted_a)) * 100
 
-    threshold_list = np.arange(0.0001, 1, 0.0001).tolist()
+    # create or write to csv file of weighted average values
+    out_df = pd.DataFrame({'hour': [target_hour], 'weighted_av_a': [weighted_av_a], 'av_a': [np.nanmean(a)],
+                           'len_grids': [len(target_grid_list)]})
+    out_df.index = out_df.hour
+    out_df = out_df.drop(columns=['hour'])
 
-    w_av_threshold_list = []
-    len_grids_threshold_list = []
+    if os.path.isfile(csv_path):
 
-    print(' ')
-    for i in threshold_list:
-        print(i)
+        existing_df = pd.read_csv(csv_path)
 
-        # loop through different thresholds
-        temp_sa = np.ma.masked_where(sa_a <= i, sa_a)
-        temp_a = np.ma.masked_where(sa_a <= i, a)
+        existing_df.index = existing_df.hour
+        existing_df = existing_df.drop(columns=['hour'])
 
-        weighted_temp_a = (temp_sa / np.nansum(temp_sa)) * temp_a
-
-        weighted_av_temp_a = np.nansum(weighted_temp_a)
-        w_av_threshold_list.append(weighted_av_temp_a)
-
-        len_grids_temp = temp_sa.count() + np.count_nonzero(~np.isnan(temp_a.data)) - np.count_nonzero(
-            ~np.isnan(temp_a.mask))
-        len_grids_threshold_list.append(len_grids_temp)
-
-    """
-
-    fig, ax1 = plt.subplots(figsize=(10,6))
-    ax2 = ax1.twinx()
-
-    plt.title(str(target_DOY) + ' ' + str(target_hour).zfill(2))
-
-    ax1.scatter(threshold_list, w_av_threshold_list, c='r', marker='.')
-
-    ax1.set_ylabel('Weighted Average UM100 $Q_{H}$ ($W$ $m^{2}$)')
-
-    ax2.set_ylabel('# of UM100 grids')
-
-    ax1.set_xlabel('SA Weight Threshold')
-
-    ax2.spines['left'].set_color('r')
-
-    ax2.spines['right'].set_color('b')
-
-    ax2.tick_params(which='both', color='blue')
-    ax1.tick_params(axis='y', which='both', color='red')
-    ax1.yaxis.label.set_color('r')
-    ax2.yaxis.label.set_color('b')
-
-    ax1.tick_params(axis='y', colors='r')
-    ax2.tick_params(axis='y', colors='b')
-
-    ax2.scatter(threshold_list, len_grids_threshold_list, c='b', marker='.')
-
-    current_path = os.getcwd().replace('\\', '/') + '/plots/changing_thresholds/' + model + '/'
-    plt.savefig(current_path + path + '_' + str(target_hour).zfill(2) + '.png', layout='tight', dpi=300)
-
-    """
-
-    print('end')
+        new_df = pd.concat([existing_df, out_df])
+        new_df.to_csv(csv_path)
 
 
+
+    else:
+        out_df.to_csv(csv_path)
+
+    # plot the data in real world coords
+    fig = plt.figure(figsize=(10, 10))
+
+    ax = fig.add_subplot(111, projection=ccrs.epsg(32631))
+
+    # plot the land cover map
+    landcover_raster_filepath = 'C:/Users/beths/OneDrive - University of Reading/Model_Eval/QGIS/Elliott/LandUseMM_7classes_32631.tif'
+    landcover_raster = rasterio.open(landcover_raster_filepath)
+    # color_list_lc = ["white", "dimgrey", "lightgrey", "deepskyblue", "lawngreen", "darkgreen", "limegreen", "olive"]
+    color_list_lc = ["white", "black", "dimgrey", "white", "lightgrey", "lightgrey", "lightgrey", "lightgrey"]
+    # make a color map of fixed colors
+    cmap_lc = colors.ListedColormap(color_list_lc)
+    bounds_lc = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    norm_lc = colors.BoundaryNorm(bounds_lc, cmap_lc.N)
+    rasterio.plot.show(landcover_raster, ax=ax, cmap=cmap_lc, norm=norm_lc, interpolation='nearest', alpha=0.5)
+
+    # plot paths
+    scint_shp_file_path = 'D:/Documents/scint_plots/scint_plots/sa_position_and_lc_fraction/scint_path_shp/BCT_IMU.shp'
+    df_12 = geopandas.read_file(scint_shp_file_path)
+    df_12.plot(edgecolor='red', ax=ax, linewidth=3.0)
+
+    # Plot observation SA
+    # open SA file
+    sa_file = 'D:/Documents/scint_UM100/scint_UM100/SA_134/BCT_IMU_15000_2016_134_' + str(target_hour).zfill(
+        2) + '_00.tif'
+    raster = rasterio.open(sa_file)
+    raster_array = raster.read()
+    # make all 0 vals in array nan
+    raster_array[raster_array == 0.0] = np.nan
+    # force non-zero vals to be 1
+    bool_arr = np.ones(raster_array.shape)
+    # remove nans in bool array
+    nan_index = np.where(np.isnan(raster_array))
+    bool_arr[nan_index] = 0.0
+    # plot
+    rasterio.plot.show(bool_arr, contour=True, transform=raster.transform, contour_label_kws={}, ax=ax, zorder=10)
+
+    target_grid_coords['grid'] = target_grid_coords['grid'].astype(int)
+
+    # plot the grid boundry box polygons
+    grid_dir = 'D:/Documents/scint_UM100/scint_UM100/grid_coords/grid_coord_lookup/grid_polygons/UM' + model.split('m')[
+        0] + '_shapes/'
+    for grid in target_grid_coords.grid.to_list():
+        grid_file_path = grid_dir + str(grid) + '.gpkg'
+
+        try:
+            assert os.path.isfile(grid_file_path)
+        except:
+            print('end')
+
+        grid_gpkg = geopandas.read_file(grid_file_path)
+        grid_gpkg.boundary.plot(ax=ax, color='skyblue')
+
+        # ax.annotate(text=grid_file_path.split('.')[0].split('/')[-1], xy=grid_gpkg.iloc[0].geometry.centroid.coords[0])
+
+    t = plt.figtext(0.35, 0.83, 'n grids = ' + str(len(target_grid_list)) + '\n' + 'W av = ' + str(round(weighted_av_a, 1)))
+    t.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor=None))
+
+    # plot the UM data
+    # from stackoverflow issue: https://stackoverflow.com/questions/62346854/how-to-convert-projection-x-and-y-coordinate-in-netcdf-iris-cube-to-lat-lon
+    # get UM coords
+    proj_x = cube.coord("grid_longitude").points
+    proj_y = cube.coord("grid_latitude").points
+
+    # get UM coord systems
+    cs_nat = cube.coord_system()
+    cs_nat_cart = cs_nat.as_cartopy_projection()
+
+    sa_a_norm = (sa_a - np.nanmin(sa_a)) / (np.nanmax(sa_a) - np.nanmin(sa_a))
+    sa_a_norm[np.isnan(sa_a_norm)] = 0
+
+    im = ax.pcolormesh(proj_x,
+                       proj_y,
+                       a,
+                       transform=cs_nat_cart,
+                       cmap='jet',
+                       alpha=sa_a_norm,
+                       vmin=150,
+                       vmax=500)
+
+    plt.colorbar(im, pad=0.01)
+
+    # limits which stay constant between and which suit both day's SAs
+    ax.set_xlim(283000, 287000)
+    ax.set_ylim(5711000, 5718000)
+
+    plt.title('DOY 134: ' + str(target_hour).zfill(2))
+
+    current_path = os.getcwd().replace('\\', '/') + '/plots/'
+
+    plt.savefig(current_path + model + '/' + path + '_' + str(target_hour).zfill(2) + '.png', bbox_inches='tight')
 
 print('end')
-
-"""
-plt.figure()
-plt.title('UM100 $Q_H$')
-# plt.imshow(a, vmin=150, vmax=500)
-plt.imshow(a)
-plt.colorbar()
-plt.xlim(390, 420)
-plt.ylim(445, 400)
-
-plt.figure()
-plt.title('UM100 Grid: SA %')
-plt.imshow(sa_a)
-plt.colorbar()
-plt.xlim(390, 420)
-plt.ylim(445, 400)
-
-sa_a_norm = (sa_a - np.nanmin(sa_a)) / (np.nanmax(sa_a) - np.nanmin(sa_a))
-sa_a_norm[np.isnan(sa_a_norm)] = 0
-
-plt.figure()
-plt.title('weighted UM100 $Q_H$')
-# plt.imshow(a, alpha=sa_a_norm, vmin=150, vmax=500)
-plt.imshow(a, alpha=sa_a_norm)
-plt.colorbar()
-plt.xlim(390, 420)
-plt.ylim(445, 400)
-plt.annotate(str(round(weighted_av_a, 1)), xy=(0.05, 0.95), xycoords='axes fraction')
-"""
-
-# no map no alpha real world coords
-
-"""
-
-# plot the data in real world coords
-# from stackoverflow issue: https://stackoverflow.com/questions/62346854/how-to-convert-projection-x-and-y-coordinate-in-netcdf-iris-cube-to-lat-lon
-fig = plt.figure(figsize=(10, 10))
-
-ax = fig.add_subplot(111, projection=ccrs.epsg(32631))
-
-# open SA file
-sa_file = 'D:/Documents/scint_UM100/scint_UM100/SA_134/BCT_IMU_15000_2016_134_' + str(target_hour).zfill(2) + '_00.tif'
-raster = rasterio.open(sa_file)
-raster_array = raster.read()
-# make all 0 vals in array nan
-raster_array[raster_array == 0.0] = np.nan
-# force non-zero vals to be 1
-bool_arr = np.ones(raster_array.shape)
-# remove nans in bool array
-nan_index = np.where(np.isnan(raster_array))
-bool_arr[nan_index] = 0.0
-
-rasterio.plot.show(bool_arr, contour=True, transform=raster.transform, contour_label_kws={}, ax=ax, zorder=10)
-
-grid_dir = 'D:/Documents/scint_UM100/scint_UM100/grid_coords/grid_coord_lookup/grid_polygons/UM' + model.split('m')[0] + '_shapes/'
-for grid in target_grid_coords.grid.to_list():
-    grid_file_path = grid_dir + str(int(grid)) + '.gpkg'
-    
-    
-    if os.path.isfile(grid_file_path):
-        pass
-    else:
-        print(grid_file_path)
-        
-    grid_gpkg = geopandas.read_file(grid_file_path)
-    grid_gpkg.boundary.plot(ax=ax, color='skyblue')
-
-
-
-# get UM coords
-proj_x = cube.coord("grid_longitude").points
-proj_y = cube.coord("grid_latitude").points
-
-# get UM coord systems
-cs_nat = cube.coord_system()
-cs_nat_cart = cs_nat.as_cartopy_projection()
-
-im = ax.pcolormesh(proj_x,
-                   proj_y,
-                   a,
-                   transform=cs_nat_cart,
-                   cmap='jet')
-"""
